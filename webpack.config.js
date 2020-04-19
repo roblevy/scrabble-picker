9e987411867583bcadc8fd1dfbc27507e12f7172
Rob Levy <robert.g.levy@gmail.com>
2020-04-19 19:22:14 +0100

    Basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: process.env.ENVIRONMENT || 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+}
