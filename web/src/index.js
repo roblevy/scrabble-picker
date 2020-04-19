fb22ec997e444148420c2e8e258d2cfdf33440c8
Rob Levy <robert.g.levy@gmail.com>
2020-04-19 20:04:34 +0100

    CORS fails API call

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/')
+      .then(res => this.setState({
+        api_response: res
+      }))
+  }
   render() {
     return (
       <div>
         <p>This is JSX from React!</p>
+        <p>
+          {
+            this.state.api_response ?
+              'Waiting...' :
+              this.state.api_response
+          }
+        </p>
       </div>
     );
   }
