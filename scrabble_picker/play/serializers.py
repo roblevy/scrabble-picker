7bc07d304eeabcfc9707c4d244b6608ef9291ad7
Rob Levy <robert.g.levy@gmail.com>
2021-01-30 17:43:12 +0000

    Attempt to make serializers work

--- scrabble_picker/play/serializers.py
+++ scrabble_picker/play/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
 from scrabble_picker.play.models import Game
+from uuid import uuid4
 
 
 class GameSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Game
-        fields = ('game_code', )
+    model = Game
+    fields = {"game_code": serializers.CharField(max_length=5)}
+
+    def create(self):
+        game_code = uuid4().hex[:4]
+        return Game.objects.create(game_code=game_code)
