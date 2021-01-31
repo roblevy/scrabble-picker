cf7dadb5dc8618dc6e93476d484c135bf2b6b7ee
Rob Levy <rob.levy@limejump.com>
2021-01-31 19:05:51 +0000

    New game button

--- frontend/src/views/GamesIndex.vue
+++ frontend/src/views/GamesIndex.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="games-index">
     <ol>
-      <li
-        v-for="game in games"
-        :key="game.game_code"
-      ><a :href="`/games/${game.game_code}/`">{{ game.game_code }}</a></li>
+      <li v-for="game in games" :key="game.game_code">
+        <a :href="`/games/${game.game_code}/`">{{ game.game_code }}</a>
+      </li>
     </ol>
+    <button v-on:click="handleNewClick">New</button>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'GamesIndex',
-  data () {
+  name: "GamesIndex",
+  data() {
     return {
       games: [],
-    }
+    };
   },
-  mounted () {
+  mounted() {
     axios
-      .get('http://localhost:8000/games/')
-      .then(response => (this.games = response.data))
-  }
-}
+      .get("http://localhost:8000/games/")
+      .then((response) => (this.games = response.data));
+  },
+  methods: {
+    handleNewClick: function() {
+      axios.post("http://localhost:8000/games/").then((response) => {
+        console.log(response.data);
+        this.$router.push({
+          name: "GamesShow",
+          params: { id: response.data.game_code },
+        });
+      });
+      console.log("Click");
+    },
+  },
+};
 </script>
