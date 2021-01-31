6ac96737afddb8b7b4cf64cf0ae450d3baf74145
Rob Levy <rob.levy@limejump.com>
2021-01-31 19:22:21 +0000

    Limit draw_letters count by available letters

--- frontend/src/views/GamesShow.vue
+++ frontend/src/views/GamesShow.vue
@@ -2,52 +2,59 @@
   <div id="games-index">
     <h1>Game Code: {{ game.game_code }}</h1>
     <h2>Letters remaining: {{ game.letters_remaining.length }}</h2>
-    <form v-on:submit.prevent>
-      <input v-model.number="letter_count" type="number" min="1" max="7">
+    <form v-on:submit.prevent v-if="game.letters_remaining.length > 0">
+      <input
+        v-model.number="letter_count"
+        type="number"
+        min="1"
+        :max="Math.min(game.letters_remaining.length, 7)"
+      />
       <button v-on:click="draw_letters">Draw letters</button>
     </form>
     <div class="last-drawn">
       <ol>
-        <li 
-          v-for="letter in letters_drawn"
-          :key="letter"
-        >{{ letter }}</li>
+        <li v-for="(letter, index) in letters_drawn" :key="`letter-${index}`">
+          {{ letter }}
+        </li>
       </ol>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'GamesShow',
-  data () {
+  name: "GamesShow",
+  data() {
     return {
       game: {
         letters_remaining: 100,
       },
       letter_count: 7,
       letters_drawn: [],
-    }
+    };
   },
-  mounted () {
+  mounted() {
     axios
       .get(`http://localhost:8000/games/${this.$route.params.id}/`)
-      .then(response => (this.game = response.data))
+      .then((response) => {
+        this.game = response.data;
+        this.letter_count = Math.min(this.game.letters_remaining.length, 7);
+      });
   },
   methods: {
-    draw_letters: function () {
-      console.log(this.letter_count);
+    draw_letters: function() {
       axios
         .post(`http://localhost:8000/games/${this.$route.params.id}/draw/`, {
-          count: this.letter_count
+          count: this.letter_count,
         })
-        .then(response => {
-            this.game = response.data.game;
-            this.letters_drawn = response.data.letters_drawn;
-        })
-    }
-  }
-}
+        .then((response) => {
+          this.game = response.data.game;
+          this.letters_drawn = response.data.letters_drawn;
+          this.letter_count = Math.min(this.game.letters_remaining.length, 7);
+        });
+    },
+  },
+};
 </script>
