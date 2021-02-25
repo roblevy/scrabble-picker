701048abe183f3ac2ffc31f35c8dbd05b01b3daa
Rob Levy <rob.levy@limejump.com>
2021-02-25 14:35:20 +0000

    We're not doing index.html in backend any more

--- scrabble_picker/core/views.py
+++ scrabble_picker/core/views.py
@@ -10,25 +10,23 @@ from rest_framework.renderers import StaticHTMLRenderer
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse('Hello world')
-
-
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
 
-    @action(detail=True, methods=['get', 'post'])
+    @action(detail=True, methods=["get", "post"])
     def draw(self, request, *args, **kwargs):
-        '''
+        """
         Draw `letters_count` letters from the bag
-        '''
+        """
         game = self.get_object()
         drawn_letters = []
-        for i in range(request.data['count']):
+        for i in range(request.data["count"]):
             letters_remaining = game.letters_remaining
             drawn_letter = random.choice(letters_remaining)
-            game.letters_remaining = letters_remaining.replace(drawn_letter, '', 1)
+            game.letters_remaining = letters_remaining.replace(drawn_letter, "", 1)
             drawn_letters.append(drawn_letter)
         game.save()
-        return Response({ "game": GameSerializer(game).data, "letters_drawn": drawn_letters })
+        return Response(
+            {"game": GameSerializer(game).data, "letters_drawn": drawn_letters}
+        )
