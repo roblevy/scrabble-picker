1fc99c3195ac7bf815bb7807f13162c01a1a67a4
Rob Levy <rob.levy@limejump.com>
2021-02-26 21:14:17 +0000

    Django URLs need a slash after

--- scrabble_picker/urls.py
+++ scrabble_picker/urls.py
@@ -8,6 +8,6 @@ router.register(r'games', views.GameViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
