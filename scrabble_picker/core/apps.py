f9ed9e4f7e50223a3e3ebd8b56082fb2baa73456
Rob Levy <rob.levy@limejump.com>
2021-02-26 08:57:16 +0000

    Tell nginx that Django app is ready

--- scrabble_picker/core/apps.py
+++ scrabble_picker/core/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+from pathlib import Path
 
 
 class ScrabblePicker(AppConfig):
     name = 'scrabble_picker.core'
+
+    def ready(self):
+        Path("/tmp/app-initialized").touch()
